039197a6e1a25c0b836eb81e66949df3bc1376df
Derick Castro <derick1castro@gmail.com>
2022-06-16 18:33:34 -0300

    studies about react.js

---
+++ primeiro-app-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/button/button';
+import Cards from './components/cards/cards'
+
+function App() {
+  return (
+    <>
+      <Button />
+      <Cards />
+    </>
+  )
+}
+
+export default App;
